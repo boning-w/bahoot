af21fdef9b876a58a4e1d3a78b0077966efc717c
boningggg <wang.boning@outlook.com>
2024-01-12 21:21:35 +1100

    backend-feat: add swagger to design and document bahoot backend apis

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,42 @@
-const express = require("express");
+const express = require("express"),
+  swaggerJsdoc = require("swagger-jsdoc"),
+  swaggerUi = require("swagger-ui-express");
+
+const options = {
+  failOnErrors: true, // Whether or not to throw when parsing errors. Defaults to false.
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      version: "0.0.0",
+      title: "Bahoot CRUD Server",
+      description: "Server that powers the Bahoot frontend",
+    },
+  },
+  apis: ["./src/*.js"], // files containing annotations as above
+};
+
 const app = express();
 const port = 4000;
 
+const swaggerSpec = swaggerJsdoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+/**
+ * @swagger
+ * tags:
+ *   name: Test
+ *   description: Only for testing backend is running
+ * /test:
+ *   get:
+ *     summary: Test if backend is running
+ *     tags: [Test]
+ *     responses:
+ *       200:
+ *         description: Successful response with plain text content "Test!"
+ *         content:
+ *           text/plain:
+ *             example: Test!
+ */
 app.get("/test", (req, res) => {
   res.send("Test!");
 });
