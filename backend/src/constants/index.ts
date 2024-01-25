7956a3be71bb43b5db6f72dcf3fb5ec077eb1356
boningggg <wang.boning@outlook.com>
2024-01-25 23:46:32 +1100

    backend-feat: add some comments to constants

--- backend/src/constants/index.ts
+++ backend/src/constants/index.ts
@@ -1,5 +1,23 @@
-export const API_DOCS = "/api/docs";
-export const API_TEST = "/api/test";
-export const API_AUTH_SIGNUP = "/api/auth/signup";
+/**
+ * API endpoint for accessing documentation.
+ * @type {string}
+ */
+export const API_DOCS: string = "/api/docs";
 
-export const SALT_ROUNDS = 10;
+/**
+ * API endpoint for test purposes.
+ * @type {string}
+ */
+export const API_TEST: string = "/api/test";
+
+/**
+ * API endpoint for user authentication signup.
+ * @type {string}
+ */
+export const API_AUTH_SIGNUP: string = "/api/auth/signup";
+
+/**
+ * Number of salt rounds used for password hashing.
+ * @type {number}
+ */
+export const SALT_ROUNDS: number = 10;
